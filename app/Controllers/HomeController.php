0ab73c4480bd12d17e6e98bd5c375e3c8bc66c92
Elijah <elijahsharper1@gmail.com>
2018-03-21 00:32:09 +0300

    set up translator package and configure it

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -12,6 +12,8 @@ class HomeController extends Controller
 {
     public function index(Request $request, Response $response, $args)
     {
+        dump($this->c->translator->trans('headers.welcome'));
+
         return $this->c->view->render($response, 'home/index.twig', [
             'appName' => $this->c->settings['app']['name'],
         ]);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,11 +18,29 @@ $app = new Slim\App([
 
         'views' => [
             'cache' => getenv('VIEW_CACHE_DISABLED') === 'true' ? false : __DIR__ . '/../storage/views'
+        ],
+
+        'translations' => [
+          'path' => __DIR__ . '/../lang',
+          'fallback' => 'en'
         ]
     ],
 ]);
 
 $container = $app->getContainer();
+
+$container['translator'] = function ($container) {
+    $fallback = $container['settings']['translations']['fallback'];
+
+    $loader = new Illuminate\Translation\FileLoader(
+        new Illuminate\Filesystem\Filesystem(), $container['settings']['translations']['path']
+    );
+
+    $translator = new Illuminate\Translation\Translator($loader, $_SESSION['lang'] ?? $fallback);
+    $translator->setFallback($fallback);
+
+    return $translator;
+};
 
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
